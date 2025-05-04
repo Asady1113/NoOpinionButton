0e198ba38815480492c2003c7ef1876a2994bb10
浅田智哉 <asadatomoya@asadatomoyanoMacBook-Air.local>
2025-05-04 17:57:32 +0900

    feat:implement the basic infra

---
+++ NoOpinionButtonInfra/src/LambdaHandlers/NoOpinionButtonFunction/src/NoOpinionButtonFunction/Function.cs
@@ -0,0 +1,34 @@
+using System.Text.Json;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace NoOpinionButtonFunction;
+
+public class Function
+{
+    
+    /// <summary>
+    /// API Gateway からのリクエストを処理するLambdaハンドラー
+    /// </summary>
+    /// <param name="input">The event for the Lambda function handler to process.</param>
+    /// <param name="context">The ILambdaContext that provides methods for logging and describing the Lambda environment.</param>
+    /// <returns></returns>
+    public APIGatewayProxyResponse FunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
+    {
+        var response = new
+            {
+                message = "Test",
+                input = request.Body
+            };
+
+        return new APIGatewayProxyResponse
+        {
+            StatusCode = 200,
+            Body = JsonSerializer.Serialize(response),
+            Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
+        };
+    }
+}
